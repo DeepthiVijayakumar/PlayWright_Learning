3f8cc0bf432668ab29643ebcdd720f1d790b9490
DeepthiVijayakumar <deepthivijay44@gmail.com>
2026-03-01 12:23:26 +0530

    Add files via upload

---
+++ primitiveDataTypesHomeAssignment.js
@@ -0,0 +1,11 @@
+let userName = "Deepthi V S";
+console.log(typeof(userName));
+let organization = "Accenture";
+console.log(typeof(organization));
+let contactNumber = 1234567890;
+console.log(typeof(contactNumber));
+let knowsAutomation = true;
+console.log(typeof(knowsAutomation));
+let usesPlaywright;
+console.log(typeof(usesPlaywright));
+
